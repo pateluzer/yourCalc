fbb930c874ea59df67d362785793e9c144a886ec
Uzer Patel <pateluzer01@gmail.com>
2024-04-04 18:11:46 -0400

    Initial Commit

---
+++ yourCalc/yourCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  yourCalcApp.swift
+//  yourCalc
+//
+//  Created by Uzer Patel on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct yourCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
